a7d2a36a24df36607806fd8c730416cef91588d7
HarinieBTS <123437428+HarinieBTS@users.noreply.github.com>
2025-03-25 12:42:01 +0530

    changes

---
+++ OneDrive/Desktop/user management/myapp/src/components/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './Header';
+import AddUser from "./AddUser"; 
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <AddUser/>
+    </div>
+  );
+}
+
+export default App;
