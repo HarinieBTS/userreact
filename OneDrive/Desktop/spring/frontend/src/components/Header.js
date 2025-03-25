f81d797d91531bc59d464f3dbbf7b0ae1f5d1542
HarinieBTS <harinie.m2022cse@sece.ac.in>
2025-03-25 14:39:05 +0530

    changes

---
+++ OneDrive/Desktop/spring/frontend/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+const Header = () => {  
+  return (
+    <header>
+      <h1>User Management</h1>
+      <h2>HariBabu</h2>
+    </header>
+  );
+}
+
+export default Header;
